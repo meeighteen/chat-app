90e665fca3f22e3a0bd18c6b942499918849d863
Josue Mamani <josuee.ald@gmail.com>
2025-03-02 00:55:15 -0500

    update output directory

--- apps/chat-world/next.config.ts
+++ apps/chat-world/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "apps/chat-world/.next",
 };
 
 export default nextConfig;
---
+++ apps/chat-world/src/app/not-found.tsx
@@ -0,0 +1,5 @@
+// apps/chat-world/app/not-found.tsx
+export default function NotFound() {
+    return <h1>Page Not Found</h1>;
+  }
+  
