7e57cdeed0b29bb1bfc4aebfa8d998a82c6be1fd
Josue Mamani <josuee.ald@gmail.com>
2025-03-03 01:11:33 -0500

    chore: delete vercel config

--- apps/chat-world/next.config.ts
+++ apps/chat-world/next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   experimental: {
     externalDir: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
+        "@packages/ui-components": require("path").resolve(
+          __dirname,
+          "../../packages/ui-components"
+        ),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
